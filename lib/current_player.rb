29ef6049cdd2d8eafacb9b87367e18b641d30243
Wasif Khan <waasif.khaan@gmail.com>
2018-04-26 16:21:32 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
   def turn_count(board)
     count = 0
@@ -16,6 +17,26 @@
       current_player = "O"
     end
   end
+=======
+def current_player(board)
+  if turn_count(board) % 2 == 0
+  current_player = "X"  
+  else
+  current_player = "O"
+  end
+end
+  
+  
+  
+  
+  def turn_count(board)
+    count = 0
+    board.each do |position|
+      (position == "X" || position == "O")
+      count += 1
+    end  
+  end 
+>>>>>>> df334aeff205d8096ed94f04ed5840c4033c85a5
     
     def move(board, index, current_player = "X")
       board[index] = current_player
